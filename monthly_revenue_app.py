7a060bf5cd321c802fe908491df1416b7fde0549
Superior16888 <91067054+Superior16888@users.noreply.github.com>
2023-10-16 04:00:19 -0500

    Update monthly_revenue_app.py

--- monthly_revenue_app.py
+++ monthly_revenue_app.py
@@ -57,7 +57,6 @@
 # st.plotly_chart(fig)
 
 
-
 import streamlit as st
 import requests
 import io
@@ -87,10 +86,35 @@ st.markdown(
 )
 
 def monthly_revenue(year, month):
-    # Your data retrieval and processing code here
+    url = "https://mops.twse.com.tw/server-java/FileDownLoad" 
+    payload = {
+        "step": "9",
+        "functionName": "show_file2",
+        "filePath": "/t21/sii/",
+        "fileName": f"t21sc03_{year}_{month}.csv"
+    }
+    response = requests.post(url, data=payload)
+    response.encoding = 'utf-8'
+    df = pd.read_csv(io.StringIO(response.text))
+    
+    df['公司名稱'] = df['公司名稱'] + "<br>" + df['公司代號'].astype(str)
+    df_filtered = df[df['營業收入-當月營收'] > 0]
+    df_filtered = df_filtered[['產業別','公司名稱','營業收入-當月營收','營業收入-上月營收','累計營業收入-當月累計營收','累計營業收入-去年累計營收']]
+    df_filtered['營業收入-當月營收'] = (df_filtered['營業收入-當月營收'] / 100000)
+    df_filtered['營業收入-上月營收'] = df_filtered['營業收入-上月營收'] / 100000
+    df_filtered['月營收增減'] = (df_filtered['營業收入-當月營收'] - df_filtered['營業收入-上月營收']) / df_filtered['營業收入-上月營收'].abs()
+    df_filtered['月營收變動'] = df_filtered['月營收增減'] ## 
+    df_filtered['累計營業收入-當月累計營收'] = df_filtered['累計營業收入-當月累計營收'] / 100000
+    df_filtered['累計營業收入-去年累計營收'] = df_filtered['累計營業收入-去年累計營收'] / 100000
+    df_filtered['累計營收增減'] = (df_filtered['累計營業收入-當月累計營收'] - df_filtered['累計營業收入-去年累計營收']) / df_filtered['累計營業收入-去年累計營收'].abs()
+    return df_filtered
 
-# Create the Plotly figure
-fig = px.treemap(df_filtered,
+df_filtered = monthly_revenue(year, month)
+
+limit = 0.3
+df_filtered['月營收變動'] = np.where(df_filtered['月營收變動'] > limit, limit, 
+              np.where(df_filtered['月營收變動'] < -limit, -limit, df_filtered['月營收變動']))
+fig = px.treemap(df_filtered, 
                  path=[px.Constant('月營收'),'產業別','公司名稱'],
                  values='營業收入-當月營收',
                  color='月營收變動',
@@ -99,7 +123,7 @@ fig = px.treemap(df_filtered,
                  color_continuous_midpoint=0,
                  custom_data=['月營收增減','累計營收增減','累計營業收入-當月累計營收','累計營業收入-去年累計營收']) 
 fig.update_layout(autosize=True)
-fig.update_layout(margin=dict(t=30, l=10, r=10, b=5))
+fig.update_layout(margin=dict(t=30, l=10, r=10, b=5)
 fig.update_traces(hovertemplate='當月營收(億)：%{value:.0f}<br>營收增減: %{customdata[0]:.1%}<br>當年累計營收: %{customdata[2]:.0f}<br>累計營收增減: %{customdata[1]:.1%}')
 fig.update_traces(textinfo='label+percent entry')
 fig.update_traces(textfont_size=16)
@@ -108,4 +132,3 @@ fig.update_traces(textfont_size=16)
 st.markdown('<div class="centered">', unsafe_allow_html=True)
 st.plotly_chart(fig)
 st.markdown('</div>', unsafe_allow_html=True)
-
